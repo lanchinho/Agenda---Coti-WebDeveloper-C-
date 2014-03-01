5994ee7f54e9117188a5b6c531536b34d4908e10
Guilherme da Costa de Albuquerque <guilherme.albuquerque@uniriotec.br>
2014-03-01 16:53:18 -0300

    Criação do script para geração das tabelas do banco de dados.

---
+++ Scripts_tabela.sql
@@ -0,0 +1,36 @@
+﻿go
+create table Usuario(
+    IdUsuario       integer           identity,
+	Nome            nvarchar(50)      not null,
+	Login           nvarchar(50)      not null  unique,
+	Email           nvarchar(50)      not null,
+	Senha           nvarchar(50)      not null,
+	primary Key(IdUsuario))
+
+go
+create table Contato(
+    IdContato       integer            identity,
+	NomeContato     nvarchar(50)       not null,
+	EmailContato    nvarchar(50)       not null,
+	Telefone        nvarchar(50)       not null,
+	IdUsuario       integer            not null  unique,
+	primary Key(IdContato)) 
+
+go
+alter table Contato add constraint FKContatoUsuario
+foreign key (IdUsuario) references Usuario(IdUsuario)
+
+go
+create table Compromisso(
+    IdCompromisso        integer          identity,
+	Titulo               nvarchar(50)     not null,
+	Descricao            nvarchar(max)    not null,  
+	Data                 datetime         not null,
+	IdUsuario            integer          not null  unique,
+	primary key(IdCompromisso))
+
+go
+alter table Compromisso add constraint FKCompromissoUsuario
+foreign key (IdUsuario) references Usuario(IdUsuario)
+
+	
