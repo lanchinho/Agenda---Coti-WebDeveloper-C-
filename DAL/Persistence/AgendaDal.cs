33ed83b8036999e31d57118de005c184b11db8ca
Guilherme da Costa de Albuquerque <guilherme.albuquerque@uniriotec.br>
2014-03-01 17:25:05 -0300

    Commit pra Consertar umas besteiras

---
+++ DAL/Persistence/AgendaDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.Persistence
+{
+    public class AgendaDal : IDisposable
+    {
+        private Conexao Con;
+    }
+}
